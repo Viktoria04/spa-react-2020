45b4dbbf0f912fb6af555dcfd699b0c045cc4d71
fmi <fmi@fmi.uni-plovdiv.bg>
2020-01-25 16:27:41 +0200

    array map, change counts

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import QuantityInput from '../components/QuantityInput';
+
 class Home extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            counts: [1, 2, 3, 4, 5]
+        }
+    }
+    getQuantityInputs = () => {
+        const counts = this.state.counts;
 
+        const inputs = counts.map(element => {
+            return < QuantityInput
+            key={element}
+            count={element}/>
+        })
+
+        return inputs;
+    }
+    changeCounts = () => {
+        this.setState({
+            counts:[7, 2]
+        })
+    }
     render() {
         return <div className="col-md-3">
+        <button onClick={this.changeCounts} type="button" className="btn btn-primary"> Change Counts </button>
             <QuantityInput count={5} />
+            {this.getQuantityInputs()}
         </div>
     }
 }
